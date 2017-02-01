8d75dd51bd39c71729f54c6c3a9d8ae5eae4499c
cainrus <cainrus@gmail.com>
2017-02-01 08:37:39 +0200

    share todos across multiple sessions

--- reminder.plugin.zsh
+++ reminder.plugin.zsh
@@ -7,6 +7,7 @@ typeset -T -x -g TODO_TASKS todo_tasks
 typeset -a -x -g todo_colors
 typeset -i -x -g todo_color_index
 
+function load_tasks() {
 # Load previous tasks from saved file
 if [[ -e "$TODO_SAVE_TASKS_FILE" &&
       -e "$TODO_SAVE_COLOR_FILE" ]] then
@@ -19,10 +20,10 @@ else
     todo_tasks=()
     todo_color_index=1
 fi
-todo_colors=(red green yellow blue magenta cyan)
+}
 
+todo_colors=(red green yellow blue magenta cyan)
 add-zsh-hook precmd todo_display
-add-zsh-hook zshexit todo_save
 
 function todo_add_task {
     if [[ $# -gt 0 ]] then
@@ -30,18 +31,23 @@ function todo_add_task {
       # Source: http://stackoverflow.com/a/8997314/1298019
       task=$(echo -E "$@" | tr '\n' '\000' | sed -e 's:\x00\x00.*:\n:g' -e 's/\000$//' -e 's/\000/\n    /g')
       task="  - ${fg[${todo_colors[${todo_color_index}]}]}$task$fg[default]"
+	  load_tasks
       todo_tasks+="$task"
       (( todo_color_index %= ${#todo_colors} ))
       (( todo_color_index += 1 ))
+      todo_save
     fi
 }
 
 function todo_task_done {
     pattern="$1"
+	load_tasks
     todo_tasks[${(M)todo_tasks[(i)*\[3?m*${pattern}*\[39m*]}]=()
+    todo_save
 }
 
 function todo_display {
+    load_tasks
     if [[ ${#todo_tasks} -gt 0 ]] then
       print -l "$fg_bold[default]Todo :$fg_no_bold[default]" ${todo_tasks}
     fi
